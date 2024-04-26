471dae156a5498a26a2b4b5cf10318bb50def67e
mohanMERNdev <151531632+mohanMERNdev@users.noreply.github.com>
2024-04-26 00:16:31 -0700

    Create ArmstrongNumber.py

---
+++ Python/ArmstrongNumber.py
@@ -0,0 +1,10 @@
+N=(input())
+N1=int(N[0])
+N2=int(N[1])
+N3=int(N[2])
+N4=int(N[3])
+N=int(N)
+if (N1 ** 4) + (N2 ** 4) + (N3 ** 4) + (N4 ** 4) == N :
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
